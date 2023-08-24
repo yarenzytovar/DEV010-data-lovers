e7d9417084bb2762b275e702a690b15f27e3dd26
Viviana Navarro <nitoname@gmail.com>
2023-08-24 11:47:08 -0600

    filtros y diseño nuevo

--- src/main.js
+++ src/main.js
@@ -1,70 +1,108 @@
-
 //import ordenar from './data.js';
-import breakingbad from './data/breakingbad/breakingbad.js';
-import { funcionDeOrdenarDatos } from './data.js';
+import breakingbad from "./data/breakingbad/breakingbad.js";
+import { filtroCategoriaSerie, funcionDeOrdenarDatos } from "./data.js";
+//import { filtradoSerie } from './data.js';
 //CONSTANTE PARA GUARDAR TODA LA DATA
-const datosPersonajes = breakingbad
+const datosPersonajes = breakingbad;
 
 //CONSTANTES PARA ELEMENTOS DEL HTML
-const personajesGeneral = document.getElementById('cartasDePersonajes');
-const informacionPersonaje = document.getElementById('cuadroPersonaje');
-const ordenAlfabetoeAsc= document.getElementById('orden');
+const personajesGeneral = document.getElementById("cartasDePersonajes");
+const informacionPersonaje = document.getElementById("cuadroPersonaje");
+const ordenAlfabetoeAsc = document.getElementById("orden");
+const selectSerie = document.getElementById("serie");
 
 //FUNCION PARA MOSTRAR LAS IMAGENES Y PERSONAJES DE LOS PERSONAJES EN EL HTML
 function mostrarTarjeta(personajes) {
-  const tarjeta = document.createElement('div'); //CONSTANTE PARA CREAR DIVS Y GUARDARLO BAJO EL NOMBRE TARJETA
-  tarjeta.classList.add('personajes-tarjeta');//AGREGAR CLASS A LOS DIVS CREADOS, CLASS='personajes-tarjetas'
+  const tarjeta = document.createElement("div"); //CONSTANTE PARA CREAR DIVS Y GUARDARLO BAJO EL NOMBRE TARJETA
+  tarjeta.classList.add("personajes-tarjeta"); //AGREGAR CLASS A LOS DIVS CREADOS, CLASS='personajes-tarjetas'
   tarjeta.innerHTML = `
     <img src="${personajes.img}" alt="${personajes.name}" />
     <p>${personajes.name}</p>
-  `;//MOSTRAR TARJETA EN HTML CON INTERPOLACION DE CADENAS. ALT POR SI NO SE VE LA IMAGEN SE PUEDA VER EL NOMBRE
-  tarjeta.addEventListener('click', () => mostrarInfo(personajes)); //DESPUES DEL EVENTO SE REALIZA LA FUNCIÓN mostrarInfo
+  `; //MOSTRAR TARJETA EN HTML CON INTERPOLACION DE CADENAS. ALT POR SI NO SE VE LA IMAGEN SE PUEDA VER EL NOMBRE
+  tarjeta.addEventListener("click", () => mostrarInfo(personajes)); //DESPUES DEL EVENTO SE REALIZA LA FUNCIÓN mostrarInfo
   return tarjeta;
 }
 
 //FUNCION PARA MOSTRAR LA INFORMACION DEL PERSONAJE
 function mostrarInfo(personajes) {
-  const informacion = document.createElement('div');//CONSTANTE PARA CREAR DIVS Y GUARDARLO BAJO EL NOMBRE informacion
-  informacion.classList.add('divInfoPersonaje');//AGREGAR CLASS A LOS DIVS CREADOS, CLASS= 'divInfoPersonaje'
+  const informacion = document.createElement("div"); //CONSTANTE PARA CREAR DIVS Y GUARDARLO BAJO EL NOMBRE informacion
+  informacion.classList.add("divInfoPersonaje"); //AGREGAR CLASS A LOS DIVS CREADOS, CLASS= 'divInfoPersonaje'
   informacion.innerHTML = `
     <h2>${personajes.name}</h2>
     <p><strong>Birthday:</strong> ${personajes.birthday}</p>
-    <p><strong>Occupation:</strong> ${personajes.occupation.join(', ')}</p>
+    <p><strong>Occupation:</strong> ${personajes.occupation.join(", ")}</p>
     <p><strong>Nickname:</strong> ${personajes.nickname}</p>
     <p><strong>Portrayed:</strong> ${personajes.portrayed}</p>  
     <p><strong>Status:</strong> ${personajes.status}</p> 
-    <p><strong>Appearance:</strong> ${personajes.appearance.join(', ')}</p>
+    <p><strong>Appearance:</strong> ${personajes.appearance.join(", ")}</p>
     <p><strong>Category:</strong> ${personajes.category}</p>
-    <p><strong>Better Call Saul appearance:</strong> ${personajes.better_call_saul_appearance.join(', ')}</p>
-  `;//MOSTRAR TARJETA EN HTML CON INTERPOLACION DE CADENAS. ALT POR SI NO SE VE LA IMAGEN SE PUEDA VER EL NOMBRE
+    <p><strong>Better Call Saul appearance:</strong> ${personajes.better_call_saul_appearance.join(
+    ", "
+  )}</p>
+  `; //MOSTRAR TARJETA EN HTML CON INTERPOLACION DE CADENAS. ALT POR SI NO SE VE LA IMAGEN SE PUEDA VER EL NOMBRE
 
-  informacionPersonaje.innerHTML = '';
+  informacionPersonaje.innerHTML = "";
   informacionPersonaje.append(informacion);
-  informacionPersonaje.classList.add('claseInfoPersonaje');
+  informacionPersonaje.classList.add("claseInfoPersonaje");
 }
 
 //FOR OF PARA RECORRER LOS VALORES DE LA PROPIEDAD
-for (const personajes of datosPersonajes.breaking_bad)//ITERAR POR CADA ELEMENTO DE datosPersonajes. CONST personajes PARA REPRESENTAR CADA ITERACION 
-{
-  const tarjeta = mostrarTarjeta(personajes);// POR CADA BUBLE SE LLAMA A LA FUNCION mostrarTarjeta
-  personajesGeneral.append(tarjeta);//LA TARJETA CREADA SE AGREGA A personajesGeneral QUE CONTIENE EL ID DEL DIV EN EL HTML
+for (const personajes of datosPersonajes.breaking_bad) { //ITERAR POR CADA ELEMENTO DE datosPersonajes. CONST personajes PARA REPRESENTAR CADA ITERACION
+  const tarjeta = mostrarTarjeta(personajes); // POR CADA BUBLE SE LLAMA A LA FUNCION mostrarTarjeta
+  personajesGeneral.append(tarjeta); //LA TARJETA CREADA SE AGREGA A personajesGeneral QUE CONTIENE EL ID DEL DIV EN EL HTML
 }
 
 //EVENTO DE CLICK PARA REMOVER LA CLASE Y LA INFORMACION
-informacionPersonaje.addEventListener('click', () => {
-  informacionPersonaje.classList.remove('claseInfoPersonaje');
+informacionPersonaje.addEventListener("click", () => {
+  informacionPersonaje.classList.remove("claseInfoPersonaje");
 });
 
 //EVENTO PARA LA FUNCIÓN DE ORDENAR EN EL DATA.JS
-ordenAlfabetoeAsc.addEventListener('change', (event)=> { //CHANGE SE USA PARA SELECT
+ordenAlfabetoeAsc.addEventListener("change", (event) => {
+  //CHANGE SE USA PARA SELECT
   //console.log(event.target.value);
   const orden = event.target.value;
-  const dataOrdenada = funcionDeOrdenarDatos(orden,datosPersonajes);
-  
-  personajesGeneral.innerHTML = '';
+  const dataOrdenada = funcionDeOrdenarDatos(orden,datosPersonajes.breaking_bad);
+
+  personajesGeneral.innerHTML = "";
   for (const personajes of dataOrdenada) {
     const tarjeta = mostrarTarjeta(personajes);
     personajesGeneral.append(tarjeta);
   }
 });
+
+selectSerie.addEventListener("change", (event) => {
+  //console.log(event.target.value);
+  const serie = event.target.value;
+  const serieSeleccionada = filtroCategoriaSerie(serie, datosPersonajes.breaking_bad);
+
+  //const serieSeleccionada = datosPersonajes.breaking_bad.filter((personaje) => {
+  //return personaje.category.includes(serie);
+  // const serieSeleccionada = datosPersonajes.breaking_bad.filter((elem) => elem.category.includes(serie));
+  //console.log(serieSeleccionada);
+  
+  personajesGeneral.innerHTML = '';
+
+  for (const personajes of serieSeleccionada) {
+    const tarjeta = mostrarTarjeta(personajes);
+    personajesGeneral.append(tarjeta);
+  }
+});
+
+
+
+//for (const personaje of datosPersonajes.breaking_bad) {
+//const apariciones = personaje.appearance;
+  
+//let suma = 0;
+
+//for (const numero of apariciones) {
+//suma += numero;
+//}
+
+//const promedio = suma / apariciones.length;
+//console.log(`El promedio de ${personaje.name} es ${promedio}`);
+//console.log(apariciones)
+//}
+
 
