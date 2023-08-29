dc6f4ef4b9d3b63c6bd55157980b8305b811aa52
Viviana Navarro <nitoname@gmail.com>
2023-08-29 10:57:17 -0600

    porcentaje y tests

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 //import ordenar from './data.js';
 import breakingbad from "./data/breakingbad/breakingbad.js";
-import { filtroCategoriaSerie, funcionDeOrdenarDatos } from "./data.js";
+import { filtroCategoriaSerie, funcionDeOrdenarDatos,promedioBreakingBad} from "./data.js";
 //import { filtradoSerie } from './data.js';
 //CONSTANTE PARA GUARDAR TODA LA DATA
 const datosPersonajes = breakingbad;
@@ -10,6 +10,7 @@ const personajesGeneral = document.getElementById("cartasDePersonajes");
 const informacionPersonaje = document.getElementById("cuadroPersonaje");
 const ordenAlfabetoeAsc = document.getElementById("orden");
 const selectSerie = document.getElementById("serie");
+const btnPromedio = document.getElementById("promedioboton");
 
 //FUNCION PARA MOSTRAR LAS IMAGENES Y PERSONAJES DE LOS PERSONAJES EN EL HTML
 function mostrarTarjeta(personajes) {
@@ -47,7 +48,8 @@ function mostrarInfo(personajes) {
 }
 
 //FOR OF PARA RECORRER LOS VALORES DE LA PROPIEDAD
-for (const personajes of datosPersonajes.breaking_bad) { //ITERAR POR CADA ELEMENTO DE datosPersonajes. CONST personajes PARA REPRESENTAR CADA ITERACION
+for (const personajes of datosPersonajes.breaking_bad) {
+  //ITERAR POR CADA ELEMENTO DE datosPersonajes. CONST personajes PARA REPRESENTAR CADA ITERACION
   const tarjeta = mostrarTarjeta(personajes); // POR CADA BUBLE SE LLAMA A LA FUNCION mostrarTarjeta
   personajesGeneral.append(tarjeta); //LA TARJETA CREADA SE AGREGA A personajesGeneral QUE CONTIENE EL ID DEL DIV EN EL HTML
 }
@@ -62,7 +64,10 @@ ordenAlfabetoeAsc.addEventListener("change", (event) => {
   //CHANGE SE USA PARA SELECT
   //console.log(event.target.value);
   const orden = event.target.value;
-  const dataOrdenada = funcionDeOrdenarDatos(orden,datosPersonajes.breaking_bad);
+  const dataOrdenada = funcionDeOrdenarDatos(
+    orden,
+    datosPersonajes.breaking_bad
+  );
 
   personajesGeneral.innerHTML = "";
   for (const personajes of dataOrdenada) {
@@ -72,37 +77,99 @@ ordenAlfabetoeAsc.addEventListener("change", (event) => {
 });
 
 selectSerie.addEventListener("change", (event) => {
-  //console.log(event.target.value);
   const serie = event.target.value;
-  const serieSeleccionada = filtroCategoriaSerie(serie, datosPersonajes.breaking_bad);
-
-  //const serieSeleccionada = datosPersonajes.breaking_bad.filter((personaje) => {
-  //return personaje.category.includes(serie);
-  // const serieSeleccionada = datosPersonajes.breaking_bad.filter((elem) => elem.category.includes(serie));
-  //console.log(serieSeleccionada);
-  
-  personajesGeneral.innerHTML = '';
-
+  const serieSeleccionada = filtroCategoriaSerie(serie,datosPersonajes.breaking_bad);
+  personajesGeneral.innerHTML = "";
   for (const personajes of serieSeleccionada) {
     const tarjeta = mostrarTarjeta(personajes);
     personajesGeneral.append(tarjeta);
+    
+  }
+});
+
+btnPromedio.addEventListener("click", ()=> {
+  const promedioPersonajesBreakingBad = promedioBreakingBad(datosPersonajes.breaking_bad);
+
+  personajesGeneral.innerHTML = "";
+ 
+  for (const personaje of promedioPersonajesBreakingBad){
+    const promedioSerie = document.createElement("div");
+    promedioSerie.classList.add("promedioLista");
+    promedioSerie.innerHTML = `
+     <h2>${personaje.name}</h2>
+     <p>Percentage appearance in Breaking Bad:
+     <h2><strong>${personaje.promedio}%</strong></h2></p>
+     `;
+    personajesGeneral.appendChild(promedioSerie)
   }
 });
 
 
 
-//for (const personaje of datosPersonajes.breaking_bad) {
-//const apariciones = personaje.appearance;
+
+//   const promedioApariciones = listaPromedios(datosPersonajes.breaking_bad);
   
-//let suma = 0;
+//   personajesGeneral.innerHTML = "";
 
-//for (const numero of apariciones) {
-//suma += numero;
-//}
+//   for (const prom of promedioApariciones) {
+//     const promedioSerie = document.createElement("div");
+//     promedioSerie.classList.add("promedioLista");
+//     promedioSerie.innerHTML = `
+//     <h2>${prom.personaje}</h2>
+//     <p>Average appearance in Breaking Bad:
+//     <h2><strong>${prom.promedio}</strong></h2></p>
+//     `;
 
-//const promedio = suma / apariciones.length;
-//console.log(`El promedio de ${personaje.name} es ${promedio}`);
-//console.log(apariciones)
-//}
+//     personajesGeneral.append(promedioSerie);
 
 
+//   }
+// }
+// );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const promediosPersonajes = document.createElement("div"); //CONSTANTE PARA CREAR DIVS Y GUARDARLO BAJO EL NOMBRE informacion
+// promediosPersonajes.classList.add("divPromPersonaje"); //AGREGAR CLASS A LOS DIVS CREADOS, CLASS= 'divInfoPersonaje'
+// promediosPersonajes.innerHTML = `
+// <h2>${personaje.name}</h2>
+// <p><strong>Promedio:</strong> ${promedio}</p>`;
+// console.log(promediosPersonajes)
+
+// for (const personajes of promedio) {
+//   const tarjeta = mostrarTarjeta(personajes);
+//   personajesGeneral.append(tarjeta);
+// }
+
+//let precios = [100, 200, 500];
+//for (let valor of precios) {
+//const length= precios.length
+//console.log(length);
+//const promedio= valor/length;
+//console.log(promedio);
+
+
+//console.log(`1.array de los personajes ${arrayNumero}`);
+//console.log(`suma del array de los personajes ${suma}`);
+//console.log(`nombre ${personaje.name} promedio ${promedio}`);
+//personajesGeneral.innerHTML = "";
